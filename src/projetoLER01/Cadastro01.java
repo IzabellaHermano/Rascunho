e74ac32c6017052ec29c7455b5814dc639e40776
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-21 11:03:59 -0300

    Primeira Tentativa de Cadastro de Alunos

--- src/projetoLER01/Cadastro01.java
+++ src/projetoLER01/Cadastro01.java
@@ -1,8 +1,33 @@
 package projetoLER01;
 
+import java.util.Scanner;
 public class Cadastro01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeroMatricula, opicao, cadastro;
+        String nome;
+
+        System.out.println("Menu:");
+        System.out.println("\t 1 - Cadastro");
+        System.out.println("\t 2 - Consulta");
 
 
+
+        cadastro = scanner.nextInt();
+        switch (cadastro) {
+            case 1:
+            System.out.println("Nome do Aluno:");
+                nome = scanner.next();
+            System.out.println("Número da Matrícula");
+                numeroMatricula= scanner.nextInt();
+                break;
+            case 2:
+                System.out.println("Número da Matrícula " );
+                numeroMatricula = scanner.nextInt();
+
+
+
+
+        }
     }
 }
