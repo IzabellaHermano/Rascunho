bd35c5c56f454dd82e5acb84927aba5b796572e2
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-28 11:01:09 -0300

    Tentativa

---
+++ src/projetoLER01/Cadastro03.java
@@ -0,0 +1,23 @@
+package projetoLER01;
+
+import java.util.Scanner;
+
+public class Cadastro03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        do {
+            System.out.println("Usuário: \n 1- Aluno \n 2 - AQV \n 3- Coordenador");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    aluno = String
+
+            }
+
+        }while ()
+
+
+    }
+}
