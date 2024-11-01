c0a95f914b2794c9634ef2add11e500232aec69b
Izabella Hermano <izabellahermano1@gmailcom>
2024-11-01 09:08:25 -0300

    tentativas

---
+++ Rascunho/src/tentativa01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class tentativa01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int maxAlunos = 10, maxCoordenadores = 2;
+
+        String [] nomeAlunos = new String[maxAlunos];
+        String [] matriculaAlunos = new String[maxAlunos];
+        int [] codUsuarios = new int[maxAlunos];
+        String nomeAQV;
+        String matriculaAQV;
+        String [] nomesCoor = new String[maxCoordenadores];
+        String [] matriculaCoor = new  String[maxCoordenadores];
+
+        int codUsuarioAtual =1, contadorAlunos= 0, contadorCoor= 0;
+        boolean
+
+    }
+}
--- src/projetoLER01/Cadastro03.java
+++ src/projetoLER01/Cadastro03.java
@@ -6,17 +6,20 @@ public class Cadastro03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcao;
-        do {
+
+        do
             System.out.println("Usuário: \n 1- Aluno \n 2 - AQV \n 3- Coordenador");
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    aluno = String
+                    String aluno;
+                    System.out.print("Nome:");
+
 
             }
 
-        }while ()
+        
 
 
     }
